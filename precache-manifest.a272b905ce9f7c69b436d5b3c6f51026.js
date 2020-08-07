2b4eb7bbd7d2aac2d93ad8435e070e1d1f9666a7
Dloft <derek.lofton1@gmail.com>
2020-08-07 11:39:01 -0400

    Updates

---
+++ precache-manifest.a272b905ce9f7c69b436d5b3c6f51026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87835aea4c02aba7fdfc1159f60bc002",
+    "url": "/Dloft1991/GP-GamePoint.git/index.html"
+  },
+  {
+    "revision": "9b88094f170a2d50d03a",
+    "url": "/Dloft1991/GP-GamePoint.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1556d798420c76693cf1",
+    "url": "/Dloft1991/GP-GamePoint.git/static/js/2.d5d24eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dloft1991/GP-GamePoint.git/static/js/2.d5d24eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b88094f170a2d50d03a",
+    "url": "/Dloft1991/GP-GamePoint.git/static/js/main.d7888df9.chunk.js"
+  },
+  {
+    "revision": "c4c53f277c0e91ecc398",
+    "url": "/Dloft1991/GP-GamePoint.git/static/js/runtime-main.4b172358.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dloft1991/GP-GamePoint.git/static/media/logo.5d5d9eef.svg"
+  }
+]);

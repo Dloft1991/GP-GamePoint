7d21a54b340bc22bd546579d7cd606a037b4393f
Dloft <derek.lofton1@gmail.com>
2020-08-08 17:57:02 -0400

    header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div class="row">
+          <div class="col-sm-6">
+          <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div class="col-sm-6">
+          <ul>
+          <li>About</li>
+          <li>Contact</li>
+          <li>login/Sign up</li>
+        </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
